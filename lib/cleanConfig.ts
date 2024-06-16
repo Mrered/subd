2397dfb6ac5cca8744edda7fa8e8578fa0d54607
Mrered Cio <34948506+Mrered@users.noreply.github.com>
2024-06-16 05:12:31 +0000

    v3.0.12

--- lib/cleanConfig.ts
+++ lib/cleanConfig.ts
@@ -13,40 +13,52 @@ interface FullConfig {
 }
 
 export const cleanConfig = (fullConfig: FullConfig) => {
+  // Helper function to update namelist
+  const updateNamelist = (config: FullConfig) => {
+    const namelist = new Set<string>(config.proxies.map(proxy => proxy.name));
+    config['proxy-groups'].forEach(group => namelist.add(group.name));
+    return namelist;
+  };
+
   // Initialize namelist with top-level proxies and proxy-group names
-  let namelist = new Set<string>(fullConfig.proxies.map(proxy => proxy.name))
-  fullConfig['proxy-groups'].forEach(group => namelist.add(group.name))
+  let namelist = updateNamelist(fullConfig);
 
   // Loop to remove invalid proxies and empty proxy-groups
-  let previousNamelistSize
+  let previousNamelistSize: number;
   do {
-    previousNamelistSize = namelist.size
+    previousNamelistSize = namelist.size;
 
     // Remove nodes not in namelist from proxy-groups
     fullConfig['proxy-groups'].forEach((group: ProxyGroup) => {
       if (group.proxies) {
         group.proxies = group.proxies.filter(proxy => {
-          return namelist.has(proxy) || proxy.toLowerCase().includes('direct')
-        })
+          return namelist.has(proxy) || proxy.toLowerCase().includes('direct');
+        });
       }
-    })
+    });
 
     // Remove empty proxy-groups
-    fullConfig['proxy-groups'] = fullConfig['proxy-groups'].filter(group => group.proxies.length > 0)
+    fullConfig['proxy-groups'] = fullConfig['proxy-groups'].filter(group => group.proxies.length > 0);
 
     // Update namelist with new proxy-group names
-    namelist = new Set<string>(fullConfig.proxies.map(proxy => proxy.name))
-    fullConfig['proxy-groups'].forEach(group => namelist.add(group.name))
+    namelist = updateNamelist(fullConfig);
 
-  } while (namelist.size !== previousNamelistSize)
+  } while (namelist.size !== previousNamelistSize);
 
   // Filter out rules not containing the remaining proxy-group names
   if (fullConfig.rules) {
-    const proxyGroupNames = fullConfig['proxy-groups'].map(group => group.name)
-    fullConfig.rules = fullConfig.rules.filter(rule => {
-      return !rule.includes('RULE-SET') || proxyGroupNames.some(name => rule.includes(name))
-    })
+    const proxyGroupNames = fullConfig['proxy-groups'].map(group => group.name);
+    fullConfig.rules = fullConfig.rules.map(rule => {
+      if (rule.includes('RULE-SET')) {
+        if (rule.includes('🇺🇸 美国') && !proxyGroupNames.includes('🇺🇸 美国')) {
+          return rule.replace('🇺🇸 美国', '🤖️ GPT');
+        }
+      }
+      return rule;
+    }).filter(rule => {
+      return !rule.includes('RULE-SET') || proxyGroupNames.some(name => rule.includes(name));
+    });
   }
 
-  return fullConfig
+  return fullConfig;
 }
